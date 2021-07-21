4e5e96cf90eff54a3afdd1740708de8a158a4c3e
Vlad <vskrashchuk@gmail.com>
2021-07-21 21:27:19 +0300

    Show simple data output in vanilla JS

--- src/main.js
+++ src/main.js
@@ -1,46 +1,75 @@
 import { getApplicationDiv } from "./lib.js";
 
+import { getHelpText, validateTriangle } from "./triangles/validation.js";
+import { isTriangle } from "./triangles/geometry.js";
+import { getAnswerPhrase } from "./triangles/output.js";
+
+const sides = [
+  { sideName: "a", sideLength: 300 },
+  { sideName: "b", sideLength: 400 },
+  { sideName: "c", sideLength: 500 },
+];
+
+const lowerBound = 0;
+const upperBound = 1000;
+
+function renderErrorScreen(messages, helpText) {
+  const fragment = new DocumentFragment();
+
+  const errorListElement = document.createElement("ul");
+
+  messages.forEach((message) => {
+    const messageItem = document.createElement("li");
+    messageItem.innerHTML = message;
+
+    errorListElement.append(messageItem);
+  });
+
+  const helpTextElement = document.createElement("p");
+  helpTextElement.innerHTML = helpText;
+
+  fragment.append(errorListElement);
+  fragment.append(helpTextElement);
+
+  return fragment;
+}
+
+function renderAnswerScreen(answerPhrase, answer) {
+  const answerElement = document.createElement("p");
+  answerElement.innerHTML = answerPhrase;
+  answerElement.style.color = answer ? "green" : "red";
+
+  return answerElement;
+}
+
+function render(appRoot) {
+  const validationResult = validateTriangle(sides, lowerBound, upperBound);
+
+  if (!validationResult.isValid) {
+    appRoot.append(
+      renderErrorScreen(
+        validationResult.errorMessages,
+        getHelpText(lowerBound, upperBound)
+      )
+    );
+
+    return;
+  }
+
+  const answer = isTriangle(sides);
+
+  appRoot.append(renderAnswerScreen(getAnswerPhrase(sides, answer), answer));
+}
+
 function main() {
   const appDiv = getApplicationDiv("#app");
 
   if (appDiv !== null) {
-    console.log(appDiv.nodeType);
-    console.log(appDiv.nodeName);
-    console.log(appDiv.tagName);
-    console.log(appDiv.innerHTML);
+    let p = prompt("Введите сторону А");
 
-    appDiv.innerHTML += "<h1>Text</h1>";
+    console.log(p);
 
-    console.log(appDiv.innerHTML);
-
-    console.log(appDiv.outerHTML);
-
-    console.log(appDiv.data);
-    console.log(appDiv.textContent);
-
-    const h1 = appDiv.querySelector("h1");
-    h1.textContent = "<h2>New text</h2>";
-
-    console.log(appDiv.textContent);
-
-    h1.hidden = false;
-
-    h1.payload = {
-      name: "User Name",
-      roleId: 145,
-    };
-
-    h1.setAttribute("custom-attribute", "my value");
-    if (h1.hasAttribute("custom-attribute")) {
-      console.log(h1.getAttribute("custom-attribute"));
-      h1.removeAttribute("custom-attribute");
-    }
-
-    console.log(h1);
-    console.dir(h1);
-
-    appDiv.style.height = "80vh";
-    appDiv.style.border = "1px solid red";
+    render(appDiv);
   } else {
     console.log("App div not found");
   }
